6f98688984eb419f527d7c6d5dd97ffdb36a8336
Jenna Cryan <jrcryan@cs.ucsb.edu>
2016-02-29 14:30:26 -0800

    board displays correctly, added Player class

--- Reversi.py
+++ Reversi.py
@@ -15,23 +15,77 @@ AUTHOR = 'Jenna Cryan'  # author
 PROGNAME = 'reversi'    # program executable name
 VRSNSTR = 'Name     : ' + PROGNAME + '\n Version : ' + str(VERSION) + '\n Author  : ' + AUTHOR     # version info
 
+LIGHT = 'L'
+DARK = 'D'
+# move = < letter >< number > indicating < column >< row >
 
 class Reversi:
     playing = False
     light = False
+    size = 8
+    last_move = '--'
+
 
     def __init__(self, n, l):
-        self.board = Board(n)
+        self.size = n
+        self.board = Board(self.size)
         self.light = l
+        if self.light is True:
+            self.computer = Player('Dark', 'computer')
+            self.human = Player('Light', 'human')
+            self.current_player = self.computer
+        else:
+            self.computer = Player('Light', 'computer')
+            self.human = Player('Dark', 'human')
+            self.current_player = self.human
+
+    def switch_player(self):
+        if self.current_player is self.computer:
+            self.current_player = self.human
+        else:
+            self.current_player = self.computer
+
+
+# def check_move(self, row, col):
+    #     if self.board[row + 1][col] is DARK or self.board[row + 1][col] is DARK
+
+    def get_move(self):
+        move = input('play < move > : ')
+        char = move[0].isalpha() and move[0] < chr(ord(self.size))
+        num = move[1].isdigit() and move[1] < self.size
+        while not char or not num:  # or not valid:
+            move = input('play < move > : ')
+            char = move[0].isalpha() and move[0] < chr(ord(self.size))
+            num = move[1].isdigit() and move[1] < self.size
+            # valid = self.check_move(move[0], move[1])
+        return move[0], move[1]
+
+    def display_info(self):
+        print 'Move played: ' + self.last_move
+        print self.current_player.color + ' player ( ' \
+              + self.current_player.type + ' ) plays now'
+        print 'Score: ' + self.computer.color + ' ' + str(self.computer.score) \
+              + ' - ' + self.human.color + ' ' + str(self.human.score)
 
     def play(self):
         logger.info('Welcome to Reversi!')
+        logger.info('')
         self.playing = True
-        if self.light is True:
-            logger.info('It"s your turn first!')
-        # while self.playing:
         self.board.clear_board()
-        self.board.display_board()
+        # start a new game
+        while self.playing:
+            self.board.display_board()
+            self.display_info()
+            # dark goes first
+            if self.light is False:
+                self.last_move = self.get_move()
+                print 'row %d, col %d' % (self.last_move[0], self.last_move[1])
+            self.playing = False;
+            # else:
+            #     row, col = self.make_move()
+            # self.set_move(row, col)
+            # self.switch_player()
+
 
 
 class Board:
@@ -56,31 +110,39 @@ class Board:
         # set up center pieces
         center_l = self.size / 2 - 1
         center_h = self.size / 2
-        self.board[center_l][center_l] = 1
-        self.board[center_h][center_h] = 1
-        self.board[center_l][center_h] = 0
-        self.board[center_h][center_l] = 0
+        self.board[center_l][center_l] = LIGHT
+        self.board[center_h][center_h] = LIGHT
+        self.board[center_l][center_h] = DARK
+        self.board[center_h][center_l] = DARK
 
         return self.board
 
     def display_board(self):
-        print '   |  a',
+        print '     a',
         for c in range(self.size - 1):
-            print ' |  ' + chr(ord('b') + c),
-        print ' |',
+            print '  ' + chr(ord('b') + c),
         self.print_line()
         for x in range(self.size):
-            print ''
             print ' ' + str(x + 1) + ' |',
             for y in range(self.size):
-                print '  ' + str(self.board[x][y]) + ' |',
+                print self.board[x][y] + ' |',
             self.print_line()
+        print ''
         return
 
     def print_line(self):
-        print '\n   ',
-        for i in range(self.size):
-            print '-----',
+        line = '\n   +' + '---+' * self.size
+        print line[0:self.size * len(line)]
+
+
+class Player:
+    color = 'Dark'
+    type = 'human'
+    score = 0
+
+    def __init__(self, color, type):
+        self.color = color
+        self.type = type
 
 
 def main():
